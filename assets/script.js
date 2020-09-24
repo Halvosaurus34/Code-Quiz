94dd5fa3e278ac483e76846d0f2d8e0672669685
Levi Halvorson <lhalvorson34@gmail.com>
2020-09-23 18:05:28 -0600

    add js and qiuz data

---
+++ assets/script.js
@@ -0,0 +1,110 @@
+var quizData = [
+    {
+        question: 'Commonly used data types do NOT include:',
+        answer1: 'Strings',
+        answer2: 'Alerts',
+        answer3: 'Booleans',
+        answer4: 'Numbers',
+        correct: 'answer2'
+    }, {
+        question: 'The condiiton in an if / else statement is enclosed within _____ .',
+        answer1: 'Quotes',
+        answer2: 'Curly Brackets',
+        answer3: 'Parenthases',
+        answer4: 'Square Brackets',
+        correct: 'answer3'
+    }, {
+        question: 'The most popular js framework right now is:',
+        answer1: 'React.js',
+        answer2: 'Angular.js',
+        answer3: 'jQuery',
+        answer4: 'Svelte',
+        correct: 'answer1'
+    }, {
+        question: 'String values must be enclosed within ____ when being assigned to variables.',
+        answer1: 'Commas',
+        answer2: 'Curly Brackets',
+        answer3: 'Quotes',
+        answer4: 'Parenthases',
+        correct: 'answer3'
+    }, {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answer1: 'JavaScript',
+        answer2: 'Terminal/Bash',
+        answer3: 'For loops',
+        answer4: 'Console.log',
+        correct: 'answer4'
+    }, 
+
+
+];
+
+var questionEl = document.getElementById('question')
+var a_text = document.getElementById('answer1');
+var b_text = document.getElementById('answer2');
+var c_text = document.getElementById('answer3');
+var d_text = document.getElementById('answer4');
+var currentQuestion = 0;
+var correctAns = ""
+
+//loads in question
+function loadQuiz() {
+    var currentQuizData = quizData[currentQuestion];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.answer1
+    b_text.innerText = currentQuizData.answer2
+    c_text.innerText = currentQuizData.answer3
+    d_text.innerText = currentQuizData.answer4
+    correctAns = currentQuizData.correct
+
+    currentQuestion++
+}
+
+var listenEl = document.getElementById('questionCon')
+//Listens for a button click
+listenEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    if(event.target.matches("button")) {
+        console.log("Your Guess" + event.target.id)
+        console.log("Right Answer" + correctAns)
+        if (event.target.id == correctAns)  {
+            console.log(`[correctAns] you guessed correct`)
+        } else {
+            console.log("WRONG")
+            time -= 10;
+        }
+        if (currentQuestion < quizData.length) {
+            loadQuiz();
+        } else {
+        //store score and change sites
+            localStorage.score = score
+            console.log(`[storage] ${localStorage.score}`)
+            // clearInterval(intervalSet)
+                //Gameover
+            location.replace("finish.html")
+        }
+    }
+  });
+
+var navTimer = document.getElementById('navTimer');
+var time = 60
+var score = []
+localStorage.score = score
+  
+//starts the quiz timer
+function quizTimer() {
+    var intervalSet = setInterval(function() {
+        navTimer.innerText = `Time Left: ${time}`
+        time--;
+        score = time
+        if (time < 1) {
+            clearInterval(intervalSet)
+            localStorage.score = score
+            location.replace("finish.html")
+        }
+    }, 1000)
+}
+//initializes functions
+loadQuiz();
+quizTimer();
